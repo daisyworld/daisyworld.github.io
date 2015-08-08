8b43057b52a7c427616eb3f91a7856fa08e443b9
Toshiki Izumi <izumi.toshiki@gmail.com>
2015-08-09 00:17:50 +0900

    rename

--- js/scripts-core.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    // Add target="_blank" when user opens external link
-    $('a').each(function() {
-      var a = this;
-      if (a.origin !== location.origin) {
-        $(a).attr('target', '_blank');
-      }
-    });
-});
